79527bd54cd36c040da54417c340e0c647399381
Rayhan Mamdani <rmamdani@bayviewglen.ca>
2021-09-13 14:29:44 -0400

    Final Modifier

---
+++ Unit One - Introduction to Computer Science/src/week2/ExampleSeven.java
@@ -0,0 +1,20 @@
+package week2;
+
+/**
+ * final modifier
+ */
+public class ExampleSeven {
+    public static void main(String[] args) {
+     final int x = 7;
+     //x = 6; //cannot reassign a value to a final variable
+     
+     final int y;
+    y =8;
+    //y++; //cannot modify a value to a final variable once constant
+
+    //name constants using all CAPITALS_AND_UNDERSCORES
+    final int NUM_STUDENTS = 18;
+
+
+    }
+}
