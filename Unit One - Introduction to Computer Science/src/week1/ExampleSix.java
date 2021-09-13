d8a7de511e223f1d7a604a44f1137a59d0928b61
Rayhan Mamdani <rmamdani@bayviewglen.ca>
2021-09-13 14:30:54 -0400

    Operations

--- Unit One - Introduction to Computer Science/src/week1/ExampleSix.java
+++ Unit One - Introduction to Computer Science/src/week1/ExampleSix.java
@@ -11,5 +11,18 @@ public class ExampleSix {
         
         x += 1; // equivalent to x = x + 1
         y -= 1; // equivalent to y = y - 1
+
+        int s = 5;
+        s++;
+        ++s;
+        s--;
+        --s;
+        
+        int g = 3;
+        int f = 3;
+
+        int test1 = 2 * g++; //test1 = 6, g = 4 (adds after)
+        int test2 = 2* ++f;  //test2 = 8, f = 4 (adds before)
+    
     }
 }
